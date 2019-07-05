1adb9bd2f98a6fed2a72b6e146e529e09de02347
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-07-05 09:47:07 -0300

    basic for the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
