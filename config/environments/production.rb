3cf0cb74f93c61380afcd4cba25e49ea8e91e559
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-07-09 17:09:38 -0300

    fixing the name of application add top rated to botton of the page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'jera-flix.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rt-nflix.herokuapp.com' }
 end
